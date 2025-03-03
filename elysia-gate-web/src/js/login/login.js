f124b956001698fabe5c5dd6180f098544794e79
ElysiaKafka <lucifermbbgh@gmail.com>
2025-03-03 16:45:24 +0800

    迁移前端代码

--- elysia-gate-web/src/js/login/login.js
+++
@@ -1,9 +0,0 @@
-function login(){
-  alert("登录成功")
-}
-
-window.onload = function() {
-  if (window.location.search.includes("error=true")) {
-    document.getElementById("error-message").classList.remove("d-none");
-  }
-};

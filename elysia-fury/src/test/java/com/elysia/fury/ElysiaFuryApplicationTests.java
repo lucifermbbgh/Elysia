471fa3e2d0d0a7c92e03bfa4a001fd6587c49aa4
ElysiaKafka <lucifermbbgh@gmail.com>
2025-03-10 20:16:49 +0800

    chore: 添加项目配置和 Docker 部署文件
    - 新增 .gitattributes 和 .gitignore 文件，规范代码仓库管理
    - 添加 application.properties 文件，配置 Spring Boot 应用
    - 新增 Nacos 配置文件和 Docker Compose 文件，用于服务注册和容器部署
    - 为 elysia-fury 和 elysia-task两个项目添加基本配置

---
+++ elysia-fury/src/test/java/com/elysia/fury/ElysiaFuryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elysia.fury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElysiaFuryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ elysia-task/src/test/java/com/elysia/task/ElysiaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elysia.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElysiaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
